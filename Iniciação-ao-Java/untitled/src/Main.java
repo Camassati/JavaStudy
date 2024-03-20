662ad0647a76bc2405be88022957b5dae244d7fc
Gustavo Martins <gfirminocruz@gmail.com>
2024-03-20 20:56:34 -0300

    Java

---
+++ Iniciação-ao-Java/untitled/src/Main.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+    double  entrada1,entrada2,saida;
+    char processamento;
+    System.out.println("Porfravor insira o primeiro paramentro");
+        Scanner (processamento);
+    }
+}
